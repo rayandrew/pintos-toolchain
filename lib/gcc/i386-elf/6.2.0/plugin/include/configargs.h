209ff3f7bda271922c196efead1b09e834faea3c
Meng Wang <wangm12@linux5.cs.uchicago.edu>
2021-10-04 14:26:38 -0500

    other toolchain

---
+++ lib/gcc/i386-elf/6.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../src/gcc-6.2.0/configure CXXFLAGS=-fpermissive --prefix=/home/wangm12/toolchain/x86_64 --target=i386-elf --disable-multilib --disable-nls --disable-werror --disable-libssp --disable-libmudflap --with-newlib --without-headers --enable-languages=c,c++";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "i386" }, { "arch", "i386" } };
